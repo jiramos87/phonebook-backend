e842573a4e430ee11dfb71dc5e36b2c613cf37ce
jiramos87 <jiramos87@gmail.com>
2022-03-15 21:52:23 -0300

    updated to MongoDB

--- index.js
+++ index.js
@@ -1,65 +1,61 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
-
 const cors = require('cors')
-app.use(cors())
+require('dotenv').config()
+const Person = require('./models/person')
 
+app.use(express.json())
+app.use(cors())
 app.use(express.static('build'))
 
 var morgan = require('morgan')
 morgan.token('response-body', (req, res) => {return JSON.stringify(req.body)});
 app.use(morgan(':method :url :response-time :response-body'))
 
-let m = 0;
-m = m + 1;
-
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       id: 1,
+//       name: "Arto Hellas", 
+//       number: "040-123456"
+//     },
+//     { 
+//       id: 2,
+//       name: "Ada Lovelace", 
+//       number: "39-44-5323523"
+//     },
+//     { 
+//       id: 3,
+//       name: "Dan Abramov", 
+//       number: "12-43-234345"
+//     },
+//     { 
+//       id: 4,
+//       name: "Mary Poppendieck", 
+//       number: "39-23-6423122"
+//     }
+// ]
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
   })
+})
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><br />
                    <p>${new Date()}</p>`)
 })
 
-const generateId = () => {
-    const id = Math.floor(Math.random() * 10000);
-    return id
-}
+// const generateId = () => {
+//     const id = Math.floor(Math.random() * 10000);
+//     return id
+// }
   
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -77,24 +73,26 @@ app.post('/api/persons', (request, response) => {
     }
 
     const name = request.body.name
-    const personExists = persons.find(person => person.name === name)
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-        date: new Date()
-        
-    }
+    const person = new Person({
+      name: name,
+      phone: phone
+    })
 
-    if (personExists === undefined) {
-      persons = persons.concat(person)
-      response.json(person)
-      
-    } else {
-        return response.status(400).json({
-            error: 'name must be unique' 
-        })
-    }
+    //const personExists = persons.find(person => person.name === name)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+    
+
+    // if (personExists === undefined) {
+    //   person.save().then(savedPerson => {
+    //     response.json(savedPerson)
+    //   })
+    // } else {
+    //     return response.status(400).json({
+    //         error: 'name must be unique' 
+    //     })
+    // }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -110,7 +108,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 8080
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
