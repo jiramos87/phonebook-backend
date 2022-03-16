d242142d64960e906a5f6158f8888b929088b638
jiramos87 <jiramos87@gmail.com>
2022-03-16 09:44:51 -0300

    changed persons to person

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const Person = require('./models/persons')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
